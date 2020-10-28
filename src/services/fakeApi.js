a88f572df8d0198f1c4bd993f3eca25d0c7631dc
neagsg <geansg4@gmail.com>
2020-10-28 13:45:39 -0300

    Alterações na fakeAPI

--- src/services/fakeApi.js
+++ src/services/fakeApi.js
@@ -1,4 +1,8 @@
 import imageItem from '../assets/images/fake/queijo.png';
+import imageItem2 from '../assets/images/fake/leite.jpg';
+import imageItem3 from '../assets/images/fake/legumes.jpg';
+import imageItem4 from '../assets/images/fake/verduras.jpg';
+import imageItem5 from '../assets/images/fake/ovos.jpg';
 
 export default [
   {
@@ -9,39 +13,39 @@ export default [
   },
   {
     id: 2,
-    image: imageItem,
-    title: 'Queijo',
-    price: 'R$ 20,00',
+    image: imageItem2,
+    title: 'Leite',
+    price: 'R$ 15,00',
   },
   {
     id: 3,
-    image: imageItem,
-    title: 'Queijo',
-    price: 'R$ 20,00',
+    image: imageItem3,
+    title: 'Legumes',
+    price: 'R$ 18,00',
   },
   {
     id: 4,
-    image: imageItem,
-    title: 'Queijo',
-    price: 'R$ 20,00',
+    image: imageItem4,
+    title: 'Verduras',
+    price: 'R$ 15,00',
   },
   {
     id: 5,
-    image: imageItem,
-    title: 'Queijo',
-    price: 'R$ 20,00',
+    image: imageItem5,
+    title: 'Ovos',
+    price: 'R$ 30,00',
   },
   {
     id: 6,
-    image: imageItem,
-    title: 'Queijo',
-    price: 'R$ 20,00',
+    image: imageItem4,
+    title: 'Verduras',
+    price: 'R$ 15,00',
   },
   {
     id: 7,
-    image: imageItem,
-    title: 'Queijo',
-    price: 'R$ 20,00',
+    image: imageItem2,
+    title: 'Leite',
+    price: 'R$ 15,00',
   },
   {
     id: 8,
